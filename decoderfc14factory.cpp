0fd91b3c10a78f174bf91dcefc629454e23796f0
Greedysky <greedysky@163.com>
2026-03-20 15:10:39 +0800

    feat: Support for qmmp 2.4

--- decoderfc14factory.cpp
+++ decoderfc14factory.cpp
@@ -39,19 +39,25 @@ Decoder *DecoderFC14Factory::create(const QString &path, QIODevice *input)
     return new DecoderFC14(path);
 }
 
-QList<TrackInfo*> DecoderFC14Factory::createPlayList(const QString &path, TrackInfo::Parts parts, QStringList *)
+TrackInfoList DecoderFC14Factory::createPlayList(const QString &path, TrackInfo::Parts parts, QStringList *)
 {
-    TrackInfo *info = new TrackInfo(path);
+#if QMMP_VERSION_INT < 0x20400
+    TrackInfo *raw(new TrackInfo(path)), *info = raw;
+#else
+    TrackInfo raw(path), info = &raw;
+#endif
     if(parts == TrackInfo::Parts())
     {
-        return QList<TrackInfo*>() << info;
+        return {raw};
     }
 
     FC14Helper helper(path);
     if(!helper.initialize())
     {
+#if QMMP_VERSION_INT < 0x20400
         delete info;
-        return QList<TrackInfo*>();
+#endif
+        return {};
     }
 
     if(parts & TrackInfo::MetaData)
@@ -68,7 +74,8 @@ QList<TrackInfo*> DecoderFC14Factory::createPlayList(const QString &path, TrackI
         info->setValue(Qmmp::FORMAT_NAME, "Future Composer");
         info->setDuration(helper.totalTime());
     }
-    return QList<TrackInfo*>() << info;
+
+    return {raw};
 }
 
 MetaDataModel* DecoderFC14Factory::createMetaDataModel(const QString &path, bool readOnly)
